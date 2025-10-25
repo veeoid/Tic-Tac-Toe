0d760f5adf78c87ed70345e4e3d94e0914c732b6
veeoid <vc7410@rit.edu>
2025-10-24 20:40:03 -0500

    Added Winner Status

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
 let currentPlayer = 'X';
-let array = Array(9).fill(null);
+let arr = Array(9).fill(null);
+
+function getWinner(){
+    if ((arr[0]!=null && arr[0] == arr[1] && arr[0] == arr[2]) ||
+        (arr[3]!=null && arr[3] == arr[4] && arr[3] == arr[5]) ||
+        (arr[6]!=null && arr[6] == arr[7] && arr[6] == arr[8]) ||
+        (arr[0]!=null && arr[0] == arr[4] && arr[0] == arr[8]) ||
+        (arr[2]!=null && arr[2] == arr[4] && arr[2] == arr[6])){
+            return true
+        }
+    return false
+}
+
+function resetGame(){
+    for (let i=0; i<9; i++){
+        arr[i] = null;
+        document.getElementById(i).innerText = '';
+    }
+}
 
 function handleClick(el){
     const id = Number(el.id);
-    array[id] = currentPlayer;
+    if (arr[id] != null) {return;}
+    arr[id] = currentPlayer;
     el.innerText = currentPlayer;
-    currentPlayer = currentPlayer === 'X'?'Y':'X';
-    console.log(array)
+
+    if (getWinner() == true){
+        console.log(`${currentPlayer} won this game!`)
+        alert(`${currentPlayer} won this game!`);
+        // notify_alert = document.createElement('alert');
+        // notify_alert.innerText = `${currentPlayer} won this game!`;
+        // document.body.appendChild(notify_alert);
+        resetGame();
+    }
+    currentPlayer = currentPlayer === 'X'?'O':'X';
+    
+    console.log(arr)
 }
 
